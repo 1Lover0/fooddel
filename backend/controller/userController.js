ac7d2ab5519a70aafbb332393babc1d8eb1e8f83
Deepu Singh <159556385+1Lover0@users.noreply.github.com>
2024-10-14 01:28:13 +0530

    user backend

---
+++ backend/controller/userController.js
@@ -0,0 +1,17 @@
+import userModel from '../models/userModel.js'
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
+import validator from 'validator'
+
+
+// login user 
+const loginUser = async () => {}
+
+
+
+// register User
+const registerUser = async () => {}
+
+
+
+export{loginUser, registerUser}
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import cors from 'cors'
 import { connectDB } from './config/db.js';
 import foodRouter from './routes/foodRoute.js';
+import userRouter from './routes/userRoutes.js';
 
 
 
@@ -23,6 +24,7 @@ connectDB()
 // api endpoints 
 app.use("/api/food",foodRouter)
 app.use('/images',express.static('uploads'))
+app.use('api/user',userRouter)
 
 
 
