c760193dfff579c8bd33d2b69fcd83f7748c9802
Deepu Singh <159556385+1Lover0@users.noreply.github.com>
2024-10-15 14:20:50 +0530

    add to cart

--- backend/controller/cartController.js
+++ backend/controller/cartController.js
@@ -3,17 +3,18 @@ import userModel from '../models/userModel.js'
 // add Cart 
 const addToCart = async (req, res) => {
     try {
-        let userData = userModel.findOne({_id:req.body.userId})
-        let cartData = await userData.cartData
-        if (!cartData[req.body.itemId]) 
-        {
-            cartData[req.body.itemId] = 1
-        } 
-        else {
-            cartData[req.body.itemId] += 1
+        let userData = await userModel.findOne({_id:req.body.userId})
+        let cartData = await userData.cartData;
+        // console.log(req.body.itemId);
+        if (!cartData[req.body.itemId]) {
+            cartData[req.body.itemId] = 1;
+        } else {
+            cartData[req.body.itemId] += 1;
         }
+        const sData = await userModel.findByIdAndUpdate(req.body.userId,{cartData})
+        console.log(sData);
+        console.log(req.body.userId);
         
-        await userModel.findByIdAndUpdate(req.body.userId,{cartData})
         res.json({success:true, message: "Added to Cart"})
 
     } catch (error) {
