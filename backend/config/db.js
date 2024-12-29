cbad26a276b344d3934ce34415469f56fa47c8f6
Deepu Singh <159556385+1Lover0@users.noreply.github.com>
2024-12-29 09:40:09 +0530

    start begning

--- backend/config/db.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://userdeepu:9990029488deepu@cluster0.m2l86fd.mongodb.net/fooddelevery?retryWrites=true&w=majority&appName=Cluster0")
-    .then(()=>console.log('DB Connected'))
-}
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,9 @@
-import React, { useState } from 'react';
-import Navbar from './components/navbar/Navbar';
-import { Route, Routes } from 'react-router-dom';
-import Home from './pages/home/Home';
-import Cart from './pages/cart/Cart';
-import PlaceOrder from './pages/placeOrder/PlaceOrder'
-import Footer from './components/footer/Footer';
-import Login from './components/login/Login';
+import React from 'react';
 
 const App = () => {
-  
-  const[showLogin, setShowLogin] = useState(false)
   return (
-    <>
-    {showLogin?<Login setShowLogin={setShowLogin} /> : <></>}
-      <div className='app'>
-        <Navbar setShowLogin={setShowLogin} />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/cart' element={<Cart />} />
-          <Route path='/order' element={<PlaceOrder />} />
-        </Routes>
-      </div>
-      <Footer />
-    </>
-  );
-};
+    <div>App</div>
+  )
+}
 
 export default App;
