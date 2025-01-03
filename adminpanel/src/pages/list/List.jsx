132d8791cabf328a450b054c88a9ca04e06bf60f
Deepu Singh <159556385+1Lover0@users.noreply.github.com>
2025-01-03 21:53:41 +0530

    admin panel functionality

--- adminpanel/src/pages/list/List.jsx
+++ adminpanel/src/pages/list/List.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './List.css';
+import axios from 'axios'
+import { toast } from 'react-toastify';
 
 const List = () => {
+  
+  const url = "http://localhost:4000"
+
+  const[list,setList] = useState([])
+
+  const fetchList = async () => {
+    const responce = await axios.get(`${url}/api/food/list`)
+    console.log(responce.data.success);
+    
+    if (responce.data.success) {
+      setList(responce.data.data)
+    } else {
+      toast.error("Error")
+    }
+  }
+  
+  useEffect(()=> {
+
+  })
+  
   return (
-    <div>List</div>
+    <div className='list'></div>
   )
 }
 
